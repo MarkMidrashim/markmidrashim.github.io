2d1e31139d478f7a48ef546916ca5dc5188e8a5c
Marcos Franco <marcosfranco.developer@gmail.com>
2022-06-17 14:29:22 -0300

    feat(group-details): sessões
    Correções de itens e textos
    Correção de responsividade
    Implementação de novas sessões e imagens

---
+++ js/main.js
@@ -0,0 +1,68 @@
+(function($) {
+	"use strict"; // Start of use strict
+
+	// Initiate the wowjs
+	new WOW().init();
+
+	// Smooth scrolling using jQuery easing
+	$('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+		if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+			if (target.length) {
+				$('html, body').animate({
+					scrollTop: (target.offset().top - 70)
+				}, 1000, "easeInOutExpo");
+				return false;
+			}
+		}
+	});
+
+	// Closes responsive menu when a scroll trigger link is clicked
+	$('.js-scroll-trigger').click(function() {
+		$('.navbar-collapse').collapse('hide');
+	});
+
+	// Activate scrollspy to add active class to navbar items on scroll
+	$('body').scrollspy({
+		target: '#mainNav',
+		offset: 100
+	});
+
+	// Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        margin: 25,
+        dots: false,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="fas fa-chevron-left"></i>',
+            '<i class="fas fa-chevron-right"></i>'
+        ],
+        responsive: {
+            0:{
+                items:1
+            },
+            992:{
+                items:1
+            }
+        }
+    });
+
+	// Collapse Navbar
+	var navbarCollapse = function() {
+		if ($("#mainNav").offset().top > 100) {
+			$("#mainNav").addClass("navbar-shrink");
+		} else {
+			$("#mainNav").removeClass("navbar-shrink");
+		}
+	};
+
+	// Collapse now if page is not at top
+	navbarCollapse();
+	// Collapse the navbar when page is scrolled
+	$(window).scroll(navbarCollapse);
+
+})(jQuery); // End of use strict
